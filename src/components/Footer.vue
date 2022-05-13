0853cc8356fde28c659b1add9e8de554a61a94c0
OceanPresent <oceanpresent@163.com>
2022-05-13 08:53:05 +0800

    :sparkles: feat: dynamic running duration

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
     <div style="text-align: center;margin-top: 1rem;" class="prose">
+    <p>This website has running for 
+    <template v-for="(value,key) in runningDuration">
+        <span v-show="value"> {{value}}{{key}}</span><span>&nbsp;</span>
+    </template>
+    </p>
     <p>
         <a href="beian.miit.gov.cn" target="_blank">皖ICP备2021007094号 </a>
         <span> 2021-PRESENT © OceanPresent</span>
@@ -8,7 +13,20 @@
 </template>
 
 <script setup lang="ts">
-
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration)
+const starttime = 1651623106000
+const timestamp = useTimestamp({offset:0})
+const durationSecond = computed(()=>timestamp.value - starttime)
+const runningDuration = computed(()=>({
+    years:dayjs.duration(durationSecond.value).years(),
+    months:dayjs.duration(durationSecond.value).months(),
+    days:dayjs.duration(durationSecond.value).days(),
+    hours:dayjs.duration(durationSecond.value).hours(),
+    minutes:dayjs.duration(durationSecond.value).minutes(),
+    seconds:dayjs.duration(durationSecond.value).seconds(),
+}))
 </script>
 
 <style scoped>
